9c264135e6045d3fdd3fe79a32d624c908aeb66f
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-11 14:50:33 -0800

    sort appointments in chronological order

--- frontend/src/appComponents/MyProfile.js
+++ frontend/src/appComponents/MyProfile.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom'
 const axios = require('axios')
 
 const MyProfile = ({ dataFromDB }) => {
-    const [appointments, setAppointments] = useState(null)
+    const [appointments, setAppointments] = useState([])
     const [description, setDescription] = useState(null)
     const [type, setType] = useState(null)
     const [images, setImages] = useState([])
     const [file, setFile] = useState()
+    const [pastAppointments, setPastAppointments] = useState([])
+    const [futureAppointments, setFutureAppointments] = useState([])
+    const todaysDate = new Date().valueOf()
+
+
+
 
     useEffect(() => {
         axios.get(`http://localhost:5001/api/appointments`)
@@ -35,6 +41,11 @@ const MyProfile = ({ dataFromDB }) => {
         const file = e.target.files[0]
             setFile(file)
         }
+
+    useEffect(() => {
+        setFutureAppointments(appointments.filter(appointment => Date.parse(appointment.date) > todaysDate))
+        setPastAppointments(appointments.filter(appointment => Date.parse(appointment.date) < todaysDate))
+    }, [appointments]) 
     
     if(!dataFromDB) return (<p>oopsie, what're ya doin here?! You need to <Link to="/signin">sign in</Link> first!</p>)
 
@@ -48,28 +59,51 @@ const MyProfile = ({ dataFromDB }) => {
                     <h3>Upload to Gallery</h3>
                     <form id="admin-upload" onSubmit={submit}>
                     <input required={true} className="input-text" placeholder='Description of photo' onChange={e => setDescription(e.target.value)} type="text"></input>
-                    <label for="hair">Hair</label>
+                    <label>Hair</label>
                     <input id="hair" type="checkbox" name="hair" value="hair" onChange={e => setType(e.target.value)} />
-                    <label for="nails">Nails</label>
+                    <label>Nails</label>
                     <input id="nails" type="checkbox" name="nails" value="nails" onChange={e => setType(e.target.value)} />
                     <input required={true} id="client-image-input" onChange={fileSelected} type="file" accept="image/*"></input>
                     <button type="submit">Submit</button>
                     </form>
                 </div>
                 <div className="all-appointments-div">
-                    <h3>Appointments</h3>
-                    { appointments ? appointments.map(appointment => (
-                        <div key={appointment._id} className="single-appointment-div">
-                            <ul>
-                            <li>{appointment.date}</li>
-                            <li>{appointment.name}</li>
-                            <li>{appointment.number}</li>
-                            <li>{appointment.description}</li>
-                            <img src={appointment.imagePath} alt="appointment inspiration" />
-                        </ul>
-                        </div>
 
-                    )) : <p>loading appointments...</p>}
+                <div className="future-appointments-div">
+                <h3>Upcoming Appointments</h3>
+                { futureAppointments ? futureAppointments.sort((a, b) => {
+                return Date.parse(a.date) - Date.parse(b.date)
+                }).map(appointment => (
+                    <div key={appointment._id} className="single-appointment-div">
+                        <ul>
+                        <li>{appointment.date}</li>
+                        <li>{appointment.name}</li>
+                        <li>{appointment.number}</li>
+                        <li>{appointment.description}</li>
+                        <img src={appointment.imagePath} alt="appointment inspiration" />
+                        </ul>
+                    </div>
+
+                )) : <p>No upcoming appointments!</p>}
+                </div>
+                <div className="past-appointments-div">
+                <h3>Previous Appointments</h3>
+                { pastAppointments ? pastAppointments.sort((a, b) => {
+                return  Date.parse(b.date) - Date.parse(a.date)
+                }).map(appointment => (
+                    <div key={appointment._id} className="single-appointment-div">
+                        <ul>
+                        <li>{appointment.date}</li>
+                        <li>{appointment.name}</li>
+                        <li>{appointment.number}</li>
+                        <li>{appointment.description}</li>
+                        <img src={appointment.imagePath} alt="appointment inspiration" />
+                        </ul>
+                    </div>
+
+                )) : <p>No past appointments!</p>}
+                </div>
+                    
                 </div>
             </div>
         )
