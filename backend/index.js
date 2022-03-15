b20c636ab4a1d00d13c17cabd95d3bc708b5d86f
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-15 09:30:04 -0700

    adding middleware

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,13 @@ app.set('port', process.env.PORT || 5000 )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../build/index.html')))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'))
