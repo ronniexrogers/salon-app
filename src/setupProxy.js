a689e505a1e037b447241fda99c11463caa00fd4
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-21 19:20:18 -0700

    troubleshooting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware(["/api"], { target: "http://localhost:8080" })
+    createProxyMiddleware(["/*"], { target: "http://localhost:8080" })
   );
 };
