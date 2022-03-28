7f8722f3ea1df6fec6751dbe7091999747e876b4
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-28 11:54:07 -0700

    testing proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware("/api", { target: "https://ronnie-rogers-capstone-backend.herokuapp.com" })
-  );
-};
+    proxy("/api", { 
+      target: "https://ronnie-rogers-capstone-backend.herokuapp.com",
+      changeOrigin: true 
+    })
+  )
+}
