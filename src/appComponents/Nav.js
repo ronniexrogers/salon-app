122b5c7411c2e0b86e3f975152a5d450f6c9ce7d
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-29 19:24:54 -0700

    fixing error

--- src/appComponents/Nav.js
+++ src/appComponents/Nav.js
@@ -7,8 +7,7 @@ import { useNavigate } from "react-router-dom"
 import { Nav, 
         NavItem, 
         Navbar, 
-        NavbarToggler, 
-        NavbarBrand, 
+        Button, 
         Collapse 
     } from "reactstrap"
 
