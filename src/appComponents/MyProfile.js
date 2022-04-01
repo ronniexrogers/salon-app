e0d007a7c17b8ce6e82da6be42bf792c1384c3e3
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-31 21:01:59 -0700

    fixing admin section

--- src/appComponents/MyProfile.js
+++ src/appComponents/MyProfile.js
@@ -56,16 +56,16 @@ const MyProfile = ({ dataFromDB, isLoggedIn }) => {
         }
     }, [])
 
-
     useEffect(() => {
         setFutureAppointments(appointments.filter(appointment => Date.parse(appointment.date) > todaysDate))
         setPastAppointments(appointments.filter(appointment => Date.parse(appointment.date) < todaysDate))
     }, [appointments])
 
     if(!dataFromDB) return (<p>oopsie, what're ya doin here?! You need to <Link to="/signin">sign in</Link> first!</p>)
-    if(!isLoggedIn) return (<p>oopsie, what're ya doin here?! You need to <Link to="/signin">sign in</Link> first!</p>)
-    else if(dataFromDB.googleId === '114694917534994982394' || '110622259906074900624') {
 
+    else if(!isLoggedIn) return (<p>oopsie, what're ya doin here?! You need to <Link to="/signin">sign in</Link> first!</p>)
+
+    else if(dataFromDB.googleId === '114694917534994982394' || '110622259906074900624') {
         return (
             <div className="my-profile">
                 <h1>Admin Dashboard</h1>
