5e653e341af82285078f6e3c95c811a140c2bcd0
ronniexrogers <ronniexrogers@yahoo.com>
2022-04-01 15:41:08 -0700

    jhfbhbrg

--- src/appComponents/Home.js
+++ src/appComponents/Home.js
@@ -6,6 +6,7 @@ const Home = ({ isAdmin }) => {
     
     const navigate = useNavigate()
 
+    if(!isAdmin){
     return ( 
         <div className="home">
             <div className="welcome-div">
@@ -40,8 +41,43 @@ const Home = ({ isAdmin }) => {
 
             <h2>Want to book an appointment?</h2>
             <Button className="home-button" onClick={() => navigate('/appointment')} >Book now</Button>
+        </div>
+     )}
+     return (
+        <div className="home">
+        <div className="welcome-div">
+        <p>Welcome!</p>
+        <div className="welcome-div-message">
+        <h3>The official website for world renowned hair stylist & nail tech Denisse Morales.</h3>
+        </div>
+        </div>
 
-            { isAdmin === true ? <Link className="navlink" to="/admin">Admin Portal</Link> : null }
+        <UncontrolledCarousel className="homepage-carousel"
+        items={[
+            {
+            altText: 'Slide 1',
+            caption: '',
+            key: 1,
+            src: 'https://denisse-morales-bucket.s3.amazonaws.com/3f6289100527afa401c8f803dd0e39ef'
+            },
+            {
+            altText: 'Slide 2',
+            caption: '',
+            key: 2,
+            src: 'https://denisse-morales-bucket.s3.us-west-1.amazonaws.com/81c0cfdc5ed472424a73f70f30b4c17e'
+            },
+            {
+            altText: 'Slide 3',
+            caption: '',
+            key: 3,
+            src: 'https://denisse-morales-bucket.s3.us-west-1.amazonaws.com/d8f47353ac16494962def110cca77103'
+            }
+        ]}
+        />
+
+        <h2>Want to book an appointment?</h2>
+        <Button className="home-button" onClick={() => navigate('/appointment')} >Book now</Button>
+        <Link className="navlink" to="/admin">Admin Portal</Link>
         </div>
      )
 }
