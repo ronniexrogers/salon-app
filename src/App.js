4192460dfca2c29d55187f3aa2022a40a8f83673
ronniexrogers <ronniexrogers@yahoo.com>
2022-04-02 10:55:13 -0700

    testing use ref hook

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from './appComponents/About'
 import Contact from './appComponents/Contact'
 import MyProfile from './appComponents/MyProfile'
 import Footer from './appComponents/Footer'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Navigation from './appComponents/Nav'
 import Admin from './appComponents/Admin'
 const axios = require('axios')
@@ -19,36 +19,36 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userData, setUserData] = useState({})
   const [dataFromDB, setDataFromDB] = useState(null)
-  let isAdmin = false
+  const admin = useRef(false)
 
   useEffect(() => {
-    console.log(isAdmin)
+    console.log(admin.isAdmin)
     axios.get(`https://ronnie-rogers-capstone-backend.herokuapp.com/api/users/${userData.googleId}`)
     .then ((res) => {
       setDataFromDB(res.data[0])
     })
   }, [userData])
 
-  useEffect(() => {
-    if(dataFromDB && dataFromDB.googleId === '114694917534994982394') {
-      isAdmin = true
-      console.log(isAdmin)
+    if(dataFromDB && (dataFromDB.googleId === '114694917534994982394' || '110622259906074900624')) {
+      admin.isAdmin = true
+      console.log(admin.isAdmin)
     }
-  }, [])
+
+
 
   return (
     <div className="App">
       <Router>
-        <Navigation isAdmin={isAdmin} userData={ userData } setIsLoggedIn={ setIsLoggedIn } isLoggedIn={isLoggedIn} />
+        <Navigation isAdmin={admin.isAdmin} userData={ userData } setIsLoggedIn={ setIsLoggedIn } isLoggedIn={isLoggedIn} />
         <div className="content-wrap">
         <Routes>
-          <Route path="/" element={<Home isAdmin={isAdmin} />} />
+          <Route path="/" element={<Home isAdmin={admin.isAdmin} />} />
           <Route path="/signin" element={<SignIn setUserData={ setUserData } userData={ userData } setIsLoggedIn={ setIsLoggedIn } isLoggedIn={isLoggedIn} />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/appointment" element={<Appointment userData={ userData } isLoggedIn={ isLoggedIn } />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/myProfile" element={<MyProfile isAdmin={ isAdmin } isLoggedIn={ isLoggedIn } dataFromDB={ dataFromDB } />} />
+          <Route path="/myProfile" element={<MyProfile isAdmin={ admin.isAdmin } isLoggedIn={ isLoggedIn } dataFromDB={ dataFromDB } />} />
           <Route path="/admin" element={<Admin isLoggedIn={ isLoggedIn } dataFromDB={ dataFromDB } />} />
         </Routes>
         </div>
